572301657efab65ffcaf90083c842b2d9cd31ef0
Alexandre Meneghello <alexandre.meneghello@sabre.com>
2017-02-24 17:35:31 -0300

    Add project files.

---
+++ NativeAppWin/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace NativeAppWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(692, 332);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "msgs received";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(272, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(417, 313);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 313);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 332);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(692, 55);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "send msgs";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(556, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "SendCmd";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(470, 19);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "showResp";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(383, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "showCmd";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(140, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "get token";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start listening";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 387);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NativeAppWin/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.Util;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using System.Threading;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace NativeAppWin
+{
+    public partial class Form1 : Form
+    {
+
+        private static String RQ_QUEUE_SUFFIX = "_request";
+        private static String RS_QUEUE_SUFFIX = "_response";
+
+        private bool isListening = false;
+      
+
+        protected static AutoResetEvent semaphore = new AutoResetEvent(false);
+        protected static ITextMessage message = null;
+        protected static TimeSpan rcvTimeout = TimeSpan.FromSeconds(10);
+
+        private IConnection connection;
+        private ISession session;
+        private IDestination destinationRQ;
+        private IDestination destinationRS;
+        IMessageProducer producer;
+        IMessageConsumer consumer;
+
+
+        private int totMsgSent = 0;
+        private int totMsgReceived = 0;
+
+        protected String athID = null;
+
+        protected static List<String> msgCache = new List<string>();
+
+        protected static Dictionary<String, String> msgsCache = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void StartSession()
+        {
+
+            //authentication data to connect to NativeAPI Bridge RedApp (ActiveMQ Broker)
+            String user = "admin";
+            String password = "password";
+            String host = "localhost";
+            int port = int.Parse("61616");
+            String redAppID = "com.sabre.tn.redapp.example.nativeApi";
+
+            //Create connection            
+            Uri connectUri = new Uri("activemq:tcp://" + host + ":" + port);
+            IConnectionFactory factory = new NMSConnectionFactory(connectUri);
+            connection = factory.CreateConnection(user,password);
+            connection.Start();
+
+            //Start a session
+            session = connection.CreateSession();
+
+            //Get address for request / reponse queues
+            destinationRQ = SessionUtil.GetDestination(session, redAppID + RQ_QUEUE_SUFFIX);
+            destinationRS = SessionUtil.GetDestination(session, redAppID + RS_QUEUE_SUFFIX);
+
+
+            // create request producer
+            producer = session.CreateProducer(destinationRQ);
+            producer.DeliveryMode = MsgDeliveryMode.Persistent;
+
+
+
+            // create a consumee for responses queue, would receive and "parse" all messages from Bridge RedApp
+            // note that it will work "assynchronously", that is, the broker can "push" messages to client, without need to "pool"
+            consumer = session.CreateConsumer(destinationRS);
+            consumer.Listener += new MessageListener(onMessageAuto);
+            
+        }
+
+        private String getSessionTokenRQ()
+        {
+            String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><com.sabre.edge.dynamo.nativeapi:GetSessionSecurityTokenRQ xmlns:com.sabre.edge.dynamo.nativeapi=\"http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0\"/>";
+            return body;
+
+
+        }
+
+        private String executeInEmuRQ(String pCommand)
+        {
+            String body = "<?xml version = \"1.0\" encoding = \"UTF-8\" ?><com.sabre.edge.dynamo.nativeapi:ExecuteInEmuRQ xmlns:com.sabre.edge.dynamo.nativeapi=\"http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0\" showCommand=\"" + (this.checkBox1.Checked ? "true" : "false") + "\" showResponse=\"" + (this.checkBox2.Checked ? "true" : "false") + "\"><Command>" + pCommand + "</Command></com.sabre.edge.dynamo.nativeapi:ExecuteInEmuRQ>";
+            return body;
+ 
+        }
+
+        private String eventSubscriptionRQ()
+        {
+            String body = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><com.sabre.edge.dynamo.nativeapi:EventSubscriptionRQ xmlns:com.sabre.edge.dynamo.nativeapi=\"http://stl.sabre.com/POS/SRW/NextGen/nativeapi/v1.0\"><com.sabre.edge.dynamo.nativeapi:event eventName=\"EMU_RESPONSE\" state=\"PRE\" /></com.sabre.edge.dynamo.nativeapi:EventSubscriptionRQ>";
+            return body;
+        }
+
+        private void EndSession()
+        {
+            consumer.Close();
+            producer.Close();
+            session.Close();
+            connection.Close();
+        }
+
+        private void stopListening()
+        {
+            consumer.Close();
+            consumer.Listener -= null;
+            
+
+        }
+
+        private void startListening()
+        {
+            SendMessage(eventSubscriptionRQ());
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartSession();
+
+        }
+
+        private void Form1_FormClosed(object sender, EventArgs e)
+        {
+            EndSession();
+        }
+        
+
+
+        private void SendMessage(String body)
+        {
+            ITextMessage msg = session.CreateTextMessage(body);
+            producer.Send(msg);
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(body);
+            String rootName = doc.DocumentElement.Name;
+            totMsgSent++;
+            String msgID = "out->" + totMsgSent + "-" + rootName;
+            cacheMsgs(msgID, msg.Text);
+
+        }
+
+
+
+        protected void onMessageAuto(IMessage rcvdMsg)
+        {
+            ITextMessage tmpmessage = rcvdMsg as ITextMessage;
+            XmlDocument doc = new XmlDocument();
+
+            doc.LoadXml(tmpmessage.Text);
+
+            String rootName = doc.DocumentElement.Name;
+            totMsgReceived++;
+            String msgID = "in<-" + totMsgReceived + "-" + rootName;
+
+            cacheMsgs(msgID, tmpmessage.Text);
+
+
+        }
+
+
+
+        private void cacheMsgs(String msgID,String msgText)
+        {
+            msgsCache.Add(msgID, msgText);
+            addMsgList(msgID);
+
+
+        }
+
+        delegate void refreshCountCb(String text);
+        private void refreshCount(String text)
+        {
+            if (this.groupBox1.InvokeRequired)
+            {
+                refreshCountCb cb = new refreshCountCb(refreshCount);
+                this.groupBox1.Invoke(cb,new object[] { text });
+
+
+            }else
+            {
+                this.groupBox1.Text = text;
+
+            }
+        }
+
+        delegate void refreshListCb(List<String> msgs);
+        private void refreshList(List<String> msgs)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+                refreshListCb cb = new refreshListCb(refreshList);
+                this.listBox1.Invoke(cb, new object[] { msgs });
+
+
+            }else
+            {
+                this.listBox1.Items.Clear();
+                int tam = msgCache.Count;
+                int i = 0;
+                for (i = 0; i < tam; i++)
+                {
+                    this.listBox1.Items.Add("Item" + i);
+                }
+
+            }
+        }
+
+        delegate void addMsgListCb(String msgID);
+        private void addMsgList(String msgID)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+                addMsgListCb cb = new addMsgListCb(addMsgList);
+                this.listBox1.Invoke(cb, new object[] { msgID });
+
+
+            }
+            else
+            {
+                
+                this.listBox1.Items.Add(msgID);
+
+
+            }
+        }
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listBox1.SelectedIndex >= 0)
+            {
+                this.richTextBox1.Clear();
+                String msgID = this.listBox1.GetItemText(this.listBox1.GetItemText(this.listBox1.SelectedItem));
+
+                if (msgsCache.ContainsKey(msgID))
+                    this.richTextBox1.Text = msgsCache[msgID];
+                else
+                    this.richTextBox1.Text = "";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isListening)
+            {
+                startListening();
+                this.button1.Text = "listening Events";
+            }
+            isListening = !isListening;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SendMessage(getSessionTokenRQ());
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SendMessage(executeInEmuRQ(this.textBox1.Text));
+        }
+
+        
+    }
+}
